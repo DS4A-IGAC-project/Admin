f24805d2299ba34e3eebae88eecf933ed767f827
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2020-11-09 12:10:54 -0500

    added url to static files

--- IGAC/IGAC/urls.py
+++ IGAC/IGAC/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+
 
 admin.site.site_header = 'IGAC Admin'
 admin.site.site_title = 'IGAC Admin'
@@ -25,4 +27,5 @@ admin.site.index_title = 'IGAC Administration'
 urlpatterns = [
                   path('', admin.site.urls),
                   path(r'chaining/', include('smart_selects.urls')),
+                  re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
